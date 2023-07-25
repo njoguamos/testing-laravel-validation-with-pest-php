e7996b0d73502a96765dff05f0282efe3974606b
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-07-25 03:01:58 +0300

    Refactor home route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RegistrationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome')->name('home');
+Route::post('/register', RegistrationController::class)->name('register');
